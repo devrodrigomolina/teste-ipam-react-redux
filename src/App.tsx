2b535d477fd90e04cecb76f2cf0366ef77270be6
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-25 23:28:17 -0300

    :sparkles: feat: Consumindo os dados da api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./store/store";
+import { fetchEstados } from "./features/localidades/estados";
+import { fetchMunicipios } from "./features/localidades/municipios";
+import { fetchMunicipioInfo } from "./features/localidades/municipioInfo";
+import { useAppSelector } from "./features/hooks/useAppSelector";
 
-function App() {
+export default function App() {
+  const estadosState = useAppSelector((state) => state.estados);
+  const municipiosState = useAppSelector((state) => state.municipios);
+  const municipioInfos = useAppSelector((state) => state.municipioInfos);
+
+  const dispatch = useDispatch<AppDispatch>();
+  const [siglaEstado, setSiglaEstado] = useState<string>();
+  const [siglaMunicipio, setMunicipio] = useState<string>();
+
+  useEffect(() => {
+    dispatch(fetchEstados());
+    if (siglaEstado) {
+      dispatch(fetchMunicipios(siglaEstado));
+    }
+  }, [siglaEstado]);
+
+  useEffect(() => {
+    dispatch(
+      fetchMunicipioInfo(
+        siglaMunicipio
+          ?.normalize("NFD")
+          .replace(/\p{Mn}/gu, "")
+          .replace(/ /g, "-")
+      )
+    );
+  }, [siglaMunicipio]);
+
   return (
-    <div className="App">
+    <>
+      <div>
+        <select
+          value={siglaEstado}
+          onChange={(e) => setSiglaEstado(e.target.value)}
+        >
+          <option selected disabled value="">
+            Selecione o estado
+          </option>
+          {estadosState.estados.map((estado) => (
+            <option key={estado.sigla} value={estado.sigla}>
+              {estado.sigla} - {estado.nome}
+            </option>
+          ))}
+        </select>
+      </div>
 
-    </div>
+      <div>
+        <select
+          value={siglaMunicipio}
+          onChange={(e) => setMunicipio(e.target.value)}
+        >
+          <option selected disabled value="">
+            Selecione um municipio
+          </option>
+          {municipiosState.municipios.map((municipio) => (
+            <option key={municipio.id} value={municipio.nome}>
+              {municipio.nome}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {Array.isArray(municipioInfos.municipio) ? (
+        municipioInfos.municipio.map(
+          ({ microrregiao, "regiao-imediata": regiaoImediata }) => (
+            <>
+              <h1>Microrregião: {microrregiao.nome}</h1> 
+              <h1>Região Imediata: {regiaoImediata.nome}</h1>
+              <h1>Mesorregiao: {microrregiao.mesorregiao.nome}</h1> 
+              <h1>Estado: {microrregiao.mesorregiao.UF.nome}</h1>
+            </>
+          )
+        )
+      ) : (
+        <>
+          {" "}
+          <h1>Microrregião: {municipioInfos.municipio.microrregiao.nome}</h1>{" "}
+          <h1>Região Imediata: {municipioInfos.municipio["regiao-imediata"].nome}</h1>{" "}
+          <h1>Mesorregiao: {municipioInfos.municipio.microrregiao.mesorregiao.nome}</h1>{" "}
+          <h1>Estado: {municipioInfos.municipio.microrregiao.mesorregiao.UF.nome}</h1>{" "}
+        </>
+      )}
+    </>
   );
 }
-
-export default App;
